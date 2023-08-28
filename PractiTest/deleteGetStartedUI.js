59f4a2685db29e599508c9937851ab14e026d766
Baplisca <yokota0327@gmail.com>
2023-08-28 21:08:26 +0900

    added Lisence

--- PractiTest/deleteGetStartedUI.js
+++ PractiTest/deleteGetStartedUI.js
@@ -7,6 +7,7 @@
 // @match        https://prod.practitest.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=practitest.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 function deleteUI() {
